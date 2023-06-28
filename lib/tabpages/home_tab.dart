9c2b0f31aecacab6de9c5b55e35fb7809459ec2d
Nisha Mahara <nishamahara86@gmail.com>
2023-06-28 20:57:08 +0545

    map

--- lib/tabpages/home_tab.dart
+++ lib/tabpages/home_tab.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeTabPage extends StatefulWidget {
   const HomeTabPage({super.key});
@@ -7,15 +10,34 @@ class HomeTabPage extends StatefulWidget {
   State<HomeTabPage> createState() => _HomeTabPageState();
 }
 
-class _HomeTabPageState extends State<HomeTabPage> {
+class _HomeTabPageState extends State<HomeTabPage>
+{
+  GoogleMapController? newGoogleMapController;
+  final Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
 
-    return const Center(
-      child: Text(
-          "Home"
-      ),
-    );
+    return Stack(
+      children: [
 
+    GoogleMap(
+    mapType: MapType.normal,
+        myLocationEnabled: true,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller)
+    {
+    _controllerGoogleMap.complete(controller);
+    newGoogleMapController = controller;
+
+         },
+
+        )
+      ],
+    );
   }
 }
